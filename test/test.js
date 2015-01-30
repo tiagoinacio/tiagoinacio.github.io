bd18ce1a2f8bbf9e7854ef280fb51dc0ed53236b
Tiago Inácio <tiago.inacio@blip.pt>
2015-01-30 14:50:40 +0000

    add script

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../app.js');
+
+describe('GET /', function() {
+      it('respond with hello world', function(done) {
+          expect(true).toBe(true);
+        });
+});
